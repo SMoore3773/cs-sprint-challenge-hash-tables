f3b4f718d95849eea399dd77ad42d387505298a8
SMoore3773 <Smooresculpture@gmail.com>
2020-11-07 16:04:50 -0500

    comment ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,17 +3,23 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    # initialize result array, hash table, and length variable
     result = []
     length = len(a)
     table = [None] * length
 
+    # populate hash table with all values from array
     for i in a:
         index = hash(i) % length
         table[index] = i
 
+    # loop through all values in array
     for i in a:
+        # check for negative number
         if i < 0:
+            # hash the absolute value of the negative number and check it against the keys in the hash table
             if hash(abs(i)) in table:
+                # if hash of the absolute value of the negative is in the hash table, then append the absolute value of the negative to the result array
                 result.append(abs(i))
     return result
 
